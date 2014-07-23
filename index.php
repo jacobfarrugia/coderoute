f524cab59e2ce68fddc479546eaae243adf69a7f
Jacob <jacobfarrugia@gmail.com>
2014-07-24 00:01:11 +0100

    basic changes

--- index.php
+++ index.php
@@ -285,29 +285,38 @@ $strapline_test->add_variation('variation_2', '<p>Better than learning to code f
 <footer>
 <div class="container">
 	
-	<!-- COMPANY ADDRESS-->
-	<div class="col-md-5 company-details">
-		<div class="icon-top red-text">
-		    <i class="icon-fontawesome-webfont-302"></i>
-		</div>
-		PO Box 16122 Collins Street West, Victoria 8007 Australia
-	</div>
 	
 	<!-- COMPANY EMAIL-->
 	<div class="col-md-2 company-details">
 		<div class="icon-top green-text">
 		<i class="icon-fontawesome-webfont-329"></i>
 		</div>
-		 contact@designlab.co
+		 getinvolved@codroute.co.uk
 	</div>
+	
+	
+	<!-- COMPANY ADDRESS-->
+	<div class="col-md-5 company-details">
+		<?php /*
+		<div class="icon-top red-text">
+		    <i class="icon-fontawesome-webfont-302"></i>
+		</div>
+		PO Box 16122 Collins Street West, Victoria 8007 Australia
+		 */ ?>
+	</div>
+	
+		
 	
 	<!-- COMPANY PHONE NUMBER -->
 	<div class="col-md-2 company-details">
-		<div class="icon-top blue-text">
+	<?php /*	<div class="icon-top blue-text">
 		<i class="icon-fontawesome-webfont-101"></i>
 		</div>
 		+613 0000 0000
+	 * */ 
+	 ?>
 	</div>
+	 
 	
 	<!-- SOCIAL ICON AND COPYRIGHT -->
 	<div class="col-lg-3 col-sm-3 copyright">
@@ -315,12 +324,17 @@ $strapline_test->add_variation('variation_2', '<p>Better than learning to code f
 			<li><a href=""><i class="icon-facebook"></i></a></li>
 			<li><a href=""><i class="icon-twitter-alt"></i></a></li>
 			<li><a href=""><i class="icon-linkedin"></i></a></li>
+			<?php /*
 			<li><a href=""><i class="icon-behance"></i></a></li>
 			<li><a href=""><i class="icon-dribbble"></i></a></li>
+			 */
+			 ?>
 		</ul>
-		 ©2013 Zerif LLC
+		 ©2013 coderoute
 	</div>
 </div> <!-- / END CONTAINER -->
+
+
 </footer> <!-- / END FOOOTER  -->
 
 <!-- SCRIPTS -->
