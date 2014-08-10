db2abdc19933fdf3c850f5709e008409794bc847
Jacob <jacobfarrugia@gmail.com>
2014-08-10 23:30:54 +0100

    home page stuff

--- application/views/pages/home-separator-generic.php
+++
@@ -1,3 +0,0 @@
-<h2 class="container text">Do this course & you'll get a job... fow shaw</h2>
-	
-<p class="container text">Blahhh blahhh blah balahah Blahhh blahhh blah balahah Blahhh blahhh blah balahah Blahhh blahhh blah balahah Blahhh blahhh blah balahah </p>
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -115,7 +115,131 @@
 <div class="container">
 		
 		
-	<?php include('home-features.php');?>	
+	<!-- SECTION HEADER -->
+	
+	<?/*
+	<div class="section-header">
+		
+		<!-- SECTION TITLE -->
+		<h2 class="dark-text">Features</h2>
+		
+		<!-- SHORT DESCRIPTION ABOUT THE SECTION -->
+		<h6>
+			We design &amp; develop qaulity products to help small &amp; medium level business.
+		</h6>
+	</div>
+	 
+	 */?>
+	<!-- / END SECTION HEADER -->
+	
+	<!-- FEATURS -->
+	<div class="row">
+		
+		<!-- FEATURES COLUMN LEFT -->
+		<div class="col-md-6 col-sm-6">
+			
+			<!-- FEATURE -->
+			<div class="feature">
+				<div class="feature-icon">
+					<i ><img src="images/icon-zend.png"/></i>
+				</div>
+				<h5>PHP5 & Zend Framework</h5>
+				<p>
+					 
+				</p>
+			</div>
+			<!-- / END FEATURE -->
+			
+			<!-- FEATURE -->
+			<div class="feature">
+				<div class="feature-icon">
+					<i ><img src="images/icon-html.png"/></i>
+				</div>
+				<h5>HTML5, CSS3 & Javascript</h5>
+				<p>
+					 
+				</p>
+			</div>
+			<!-- / END FEATURE -->
+			
+			<!-- FEATURE -->
+			<div class="feature">
+				<div class="feature-icon">
+					<i ><img src="images/icon-mysql.png"/></i>
+				</div>
+				<h5>MySQL</h5>
+				<p>
+					 
+				</p>
+			</div>
+			<!-- / END FEATURE -->
+			
+			<!-- FEATURE -->
+			<div class="feature">
+				<div class="feature-icon">
+					<i ><img src="images/icon-git.png"/></i>
+				</div>
+				<h5>GIT</h5>
+				<p>
+					
+				</p>
+			</div>
+			<!-- / END FEATURE -->
+			
+		</div> <!-- / FEATURES COLUMN LEFT -->
+		
+		<!-- FEATURES COLUMN RIGHT -->
+		<div class="col-md-6 col-sm-6 wow">
+			<!-- FEATURE -->
+			<div class="feature">
+				<div class="feature-icon">
+					
+				</div>
+				<h5>Real world projects</h5>
+				<p>
+					By working on real world projects for real clients you'll come out with a portoflio of work that you can use to wow proprective employers. 
+				</p>
+			</div>
+			<!-- / END FEATURE -->
+			
+			<!-- FEATURE -->
+			<div class="feature">
+				<div class="feature-icon">
+					<i class="icon-handle-streamline-vector"></i>
+				</div>
+				<h5>Workshops & Talks from industry pros</h5>
+				<p>
+					 
+				</p>
+			</div>
+			<!-- / END FEATURE -->
+			
+			<!-- FEATURE -->
+			<div class="feature">
+				<div class="feature-icon">
+					<i class="icon-speech-streamline-talk-user"></i>
+				</div>
+				<h5>Agile, Scrum & Lean working practices</h5>
+				<p>
+					 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				</p>
+			</div>
+			<!-- / END FEATURE -->
+			
+			<!-- FEATURE -->
+			<div class="feature">
+				<div class="feature-icon">
+					<i class="icon-speech-streamline-talk-user"></i>
+				</div>
+				<h5>CV clinics & Interview coaching </h5>
+				<p>
+					 We work closely with some of London's top recruitment firms to provide full support service to help you get that first tech job.
+				</p>
+			</div>
+			<!-- / END FEATURE -->
+			
+		</div> <!-- / END FEATURES COLUMN RIGHT -->
+	</div> <!-- / END FEATURES -->
 		
 
 </div> <!-- / END CONTAINER -->
@@ -181,9 +305,17 @@
 ============================== -->
 
 <section class="separator-one">
-<div class="color-overlay">
+	<div class="color-overlay">
 	
-	<?php include('home-separator-cta.php');?>
+	<div class="container text">
+		
+		<h3 class="white-text" >
+			Ready to get involved...
+		</h3>
+
+		<a href="" class="btn btn-primary custom-button red-btn">Apply Now</a>
+	
+	</div>
 	
 </div>
 </section>  <!-- / END SEPARATOR -->
