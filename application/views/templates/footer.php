f99aff0699025f319b2245c8b5289f0bb4200627
Jacob <jacobfarrugia@gmail.com>
2014-09-17 17:11:00 +0100

    stuff

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,9 +1,3 @@
-<?php
-
-include "online.php";
-
-?>
-
 <!-- =========================
    FOOTER             
 ============================== -->
@@ -46,7 +40,7 @@ include "online.php";
 	if($page == ('contact' || 'apply')){
 ?>
 
-<script src="xxxjs/form-validator/jquery.form-validator.js"></script>
+<script src="js/form-validator/jquery.form-validator.js"></script>
 <script>
 	
 var $messages = $('#error-container');
@@ -100,7 +94,7 @@ function gaOptout() {
 ?>
 
 
-<?php if($online){ ?>
+<?php if(!strpos(base_url(),'localhost') ){ ?>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
