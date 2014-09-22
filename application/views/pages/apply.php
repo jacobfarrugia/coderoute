ddcdca53a30ffe7d45271cb77399364caf20062a
Jacob <jacobfarrugia@gmail.com>
2014-09-22 18:27:48 +0100

    facebook pixel

--- application/views/pages/apply.php
+++ application/views/pages/apply.php
@@ -64,6 +64,25 @@ var google_remarketing_only = false;
 </div>
 </noscript>
 
+
+
+
+<!-- Facebook Conversion Code for Application -->
+<script>(function() {
+var _fbq = window._fbq || (window._fbq = []);
+if (!_fbq.loaded) {
+var fbds = document.createElement('script');
+fbds.async = true;
+fbds.src = '//connect.facebook.net/en_US/fbds.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(fbds, s);
+_fbq.loaded = true;
+}
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6018897841084', {'value':'0.01','currency':'GBP'}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6018897841084&amp;cd[value]=0.01&amp;cd[currency]=GBP&amp;noscript=1" /></noscript>
 	
 <?php }else{ ?>	
 
